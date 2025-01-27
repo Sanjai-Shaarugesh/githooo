8f5458deb7f10bb3fa8fe5b0fb4d628e62fc615f
Sanjai-Shaarugesh <shaarugesh5@outlook.com>
2025-01-27 23:55:19 +0530

    few changes in ui&ux x  🥸

--- githooo/src/routes/profile/+page.svelte
+++ githooo/src/routes/profile/+page.svelte
@@ -98,7 +98,7 @@
               borderRadius="1.75rem"
               className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
             >
-             Githuub
+             Github 🐱
             </Button></a>
            
           </div>
