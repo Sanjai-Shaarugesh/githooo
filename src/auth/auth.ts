a3a25a9e63ea792af581d26bf00bcde2a76d1994
Sanjai-Shaarugesh <shaarugesh5@outlook.com>
2025-01-30 02:20:36 +0530

    few changes

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,30 +1,54 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
-import GitHub from '@auth/sveltekit/providers/github';
-import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
+import GitHub from '@auth/core/providers/github';
+import { GITHUB_ID, GITHUB_SECRET, AUTH_SECRET } from '$env/static/private';
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
-	providers: [
-		GitHub({
-			clientId: GITHUB_ID,
-			clientSecret: GITHUB_SECRET
-		})
-	],
+  providers: [
+    GitHub({
+      clientId: GITHUB_ID,
+      clientSecret: GITHUB_SECRET
+    })
+  ],
 
-	trustHost: true, 
+  // Use AUTH_SECRET for JWT token encryption
+  secret: AUTH_SECRET,
 
-	callbacks: {
-		async jwt({ token, account }) {
-			// Persist the access_token to the token right after signin
-			if (account) {
-				token.accessToken = account.access_token;
-			}
-			return token;
-		},
-		async session({ session, token }) {
-			// Send properties to the client
-			//@ts-ignore
-			session.access_token = token.accessToken;
-			return session;
-		}
-	}
+  // Trust host is necessary for Vercel since hostnames can change
+  trustHost: true,
+
+  session: {
+    strategy: 'jwt',
+    // JWT settings for better session persistence
+    maxAge: 30 * 24 * 60 * 60, // 30 days in seconds
+    updateAge: 24 * 60 * 60, // Refresh token after 24 hours
+  },
+
+  cookies: {
+    sessionToken: {
+      name: '__Secure-next-auth.session-token',
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        // In Vercel, ensure 'secure' is true since it's HTTPS by default
+        secure: true,
+      }
+    },
+  },
+
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      // Send properties to the client
+	  //@ts-ignore
+      session.access_token = token.accessToken as string;
+      return session;
+    }
+  }
 });
