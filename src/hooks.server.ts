5c3cbb7ffbdcac7080b88ebe22922bda6be8ff15
Sanjai-Shaarugesh <shaarugesh5@outlook.com>
2025-01-30 20:10:35 +0530

    few bugs fixed 🐞

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,2 @@
-import { handle as authHandle } from '../src/auth/auth';
+export  { handle  } from '../src/auth/auth';
 
-export const handle = async ({ event, resolve }) => {
-  // Use SvelteKitAuth's handle to process authentication
-  const response = await authHandle({ event, resolve });
-
-  // If you want to add additional middleware logic, you can do so here
-
-  return response;
-};
