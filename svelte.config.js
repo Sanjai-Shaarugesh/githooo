731df155c2e8a50b9d258be8e3d3553dcf4591c2
Sanjai-Shaarugesh <shaarugesh5@outlook.com>
2025-01-30 19:14:16 +0530

    bugs fixed d 🪲

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,9 @@ import { preprocessMeltUI, sequence } from '@melt-ui/pp';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 //import adapter from '@sveltejs/adapter-cloudflare';
-import adapter from 'sveltekit-adapter-deno';
+//import adapter from 'sveltekit-adapter-deno';
+
+import adapter from '@sveltejs/adapter-netlify';
 
 
 /** @type {import('@sveltejs/kit').Config}*/
@@ -17,6 +19,8 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
+			edge: false,
+			split: false,
 			routes: {
 				include: ['/*'],
 				exclude: ['<all>']
