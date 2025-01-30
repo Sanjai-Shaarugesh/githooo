4286cda8d80db77a05b92672a585228d42938aa5
Sanjai-Shaarugesh <shaarugesh5@outlook.com>
2025-01-30 21:14:02 +0530

    few bugs fixed 🐞

--- src/routes/profile/+server.ts
+++ src/routes/profile/+server.ts
@@ -1,12 +1,17 @@
+import type { RequestHandler } from '@sveltejs/kit';
 
-export async function GET({ locals }) {
-    //@ts-ignore
-    const session = locals.session;
-  
-    if (!session) {
-      return new Response('Unauthorized', { status: 401 });
-    }
-  
-    return new Response(JSON.stringify(session), { status: 200 });
+export const GET: RequestHandler = async ({ locals }) => {
+  //@ts-ignore
+  if (!locals.session) {
+    return new Response('Unauthorized', { status: 401 });
   }
-  
+
+  // Assuming 'session' in locals has a proper type that includes user data
+  //@ts-ignore
+  return new Response(JSON.stringify(locals.session), {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    status: 200
+  });
+};
