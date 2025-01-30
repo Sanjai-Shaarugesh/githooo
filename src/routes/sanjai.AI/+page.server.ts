4921f6590bcef6710de6d8c4ec67e1bcff50e59b
Sanjai-Shaarugesh <shaarugesh5@outlook.com>
2025-01-30 23:12:02 +0530

    it's working bro.. 🥦

--- src/routes/sanjai.AI/+page.server.ts
+++ src/routes/sanjai.AI/+page.server.ts
@@ -1,7 +1,7 @@
 import type { PageServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
-export const prerender = 'auto';
+export const prerender = false;
 
 export const load: PageServerLoad = async (event) => {
 	const session = await event.locals.auth();
