1a1444bf8dfeaed3ab21695a25bb09cf72a132fd
Sanjai-Shaarugesh <shaarugesh5@outlook.com>
2025-01-30 19:56:05 +0530

    few bugs fixed 🐞

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,31 @@
 import { preprocessMeltUI, sequence } from '@melt-ui/pp';
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { enhancedImages } from '@sveltejs/enhanced-img';
+//import adapter from '@sveltejs/adapter-cloudflare';
+//import adapter from 'sveltekit-adapter-deno';
 
-/** @type {import('@sveltejs/kit').Config} */
+//import adapter from '@sveltejs/adapter-netlify';
+//import adapter from '@sveltejs/adapter-static';
+
+
+/** @type {import('@sveltejs/kit').Config}*/
 const config = {
-    preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
-    kit: {
-        adapter: adapter({
-            // Vercel-specific configuration
-            edge: false,           // Use Node.js runtime
-            external: [],          // No external dependencies to include
-            split: false,          // Don't split your app into smaller chunks
-            isr: {
-                expiration: false  // Disable ISR by default
-            }
-        }),
-        inlineStyleThreshold: Infinity,
-        alias: {
-            '$lib/*': './src/lib/*'
-        }
-    }
-};
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
+	plugins: [enhancedImages()],
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter({
+			
+			
+		}),
+		inlineStyleThreshold: Infinity,
 
+		
+	}
+};
 export default config;
