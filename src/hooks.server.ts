a5ea9305f5d31578655c2b0a34cd82c966fb134d
Sanjai-Shaarugesh <shaarugesh5@outlook.com>
2025-01-30 20:14:27 +0530

    few bugs fixed 🐞

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,2 +1,13 @@
-export  { handle  } from '../src/auth/auth';
+// hooks.server.ts
+import { sequence } from '@sveltejs/kit/hooks';
+import { handle as authHandle } from '../src/auth/auth';
 
+// If you need to add custom middleware
+//@ts-ignore
+const customHandle = async ({ event, resolve }) => {
+  // Add any custom middleware logic here before auth
+  return resolve(event);
+};
+
+// Export the sequence of handlers
+export const handle = sequence(customHandle, authHandle);
